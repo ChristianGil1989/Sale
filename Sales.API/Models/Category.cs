106c6227a2b22d5a2af7c32eb3c697c996a264c8
Christian Gil <christiang@sciww.com>
2023-09-04 22:13:29 -0500

    Add DbContext, connection DB and firts model category

---
+++ Sales.API/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.API.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Categoría")]
+        [MaxLength(100, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Name { get; set; } = null!;
+
+        public DateTime CreationDate { get; set; }
+    }
+}
--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionSQLDB"));
+});
 
 // Add services to the container.
 
